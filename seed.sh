8622a582a929050d9618e768b5d66168e542c3e0
simplerick0 <simplerick@sigmahax.com>
2021-12-17 12:57:51 -0700

    Rename some vars, add stubs for shell and compositor roles

--- seed.sh
+++ seed.sh
@@ -3,7 +3,7 @@
 echo "Downloading SSH Key"
 install -m 700 -d /root/.ssh
 github_username="simplerick0"
-curl -s "https://api.github.com/users/${github_username}/keys" | python -c 'import json,sys;print(json.load(sys.stdin)[2]["key"])' > /root/.ssh/authorized_keys
+curl -s "https://api.github.com/users/${github_username}/keys" | python -c 'import json,sys;print(json.load(sys.stdin)[3]["key"])' > /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 
 echo "Starting SSH"
